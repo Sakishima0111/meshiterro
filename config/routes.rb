7dc2ea543a8fd58f67ff4279ac075ea4500b4de1
EC2 Default User <ec2-user@ip-172-31-1-195.ap-northeast-1.compute.internal>
2024-01-15 08:25:41 +0000

    7thcommit

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   get 'homes/about', as: 'about'
-
+  
   devise_for :users
     devise_scope :user do
     get '/users/sign_out', to: 'devise/sessions#destroy'
     end
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+  resource :favorite, only: [:create, :destroy]
   resources :post_comments, only: [:create, :destroy]
   end
 
